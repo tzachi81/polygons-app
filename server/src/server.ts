6b099311d01c9e8b4a3d59b9ea5b804cbb7f6ab8
tzachi81 <tzahih@gmail.com>
2025-08-13 11:56:41 +0300

    Fix allowed origins

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import type {CorsOptions} from 'cors';
 import polygonRoutes from './routes/polygonRoutes';
 import serverHealthRoute from './routes/serverHealthRoute';
 import * as dotenv from 'dotenv';
@@ -7,29 +8,25 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5006;
+const PORT = process.env.PORT || 5005;
 const DELAY = 5000;
 
 
 const customDelayMiddleware = (request: Request, response: Response, next: NextFunction) => {
-  if (request.method !== 'GET') setTimeout(() => {
+  setTimeout(() => {
     next();
   }, DELAY);
 };
 
-const customLogMiddleware = (request: Request, response: Response, next: NextFunction) => {
-   console.log("A new request received at " + Date.now());
-   next();
-};
-
-const devOrigins = ['http://localhost:5173', 'http://localhost:4173'];
+const devOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 const allowedOrigins = (process.env.NODE_ENV === "production") ? [process.env.ALLOWED_ORIGIN] : devOrigins;
 
 
-
-const corsOptions = {
+const corsOptions: CorsOptions = {
     origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        console.log('Incoming origin:', origin);
+        console.log('Allowed origins:', allowedOrigins);
+      if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
@@ -37,17 +34,13 @@ const corsOptions = {
     },
 };
 
-
-
 app.use(express.json());
 
-// app.use(cors(corsOptions));
-console.log('Allowed origin', process.env.ALLOWED_ORIGIN);
-app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+app.use(cors(corsOptions));
+
 app.use('/api/health', serverHealthRoute);
-app.use('/api/polygons', polygonRoutes);
 app.use('/api/polygons', customDelayMiddleware);
-app.use('/api/', customLogMiddleware);
+app.use('/api/polygons', polygonRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
